b67398b23f3ee26339c324dcadb607138f4397d5
joegit12 <joenobilione@gmail.com>
2024-09-05 10:04:33 -0400

    HW 1 complete

---
+++ Homework 1/test.java
@@ -0,0 +1,20 @@
+//always start with importing our scanner so we can use it!
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+
+System.out.println("Hellow World!");
+System.out.println(12);
+String name = "Joe";
+System.out.println(name);
+Integer age = 30;
+System.out.println("My name is " + name + " and my age is " + age);
+
+
+
+
+
+    }
+
+}
