5f800829d3566513b5a2ea8afb15d7819a7247eb
joegit12 <joenobilione@gmail.com>
2024-09-12 10:47:49 -0400

    HW2 ifelse practice

--- Homework 2/ifelsepractice.java
+++ Homework 2/ifelsepractice.java
@@ -8,12 +8,51 @@ import java.util.Scanner;
 * can be any topic you pick :) feel free to pick some obscure or niche topics!
 * good luck!
 * */
-public class Main {
+public class ifelsepractice {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+    System.out.println("What is your name?");
+    String name = sc.nextLine();
+if (name.equals("Joe")) {
+    System.out.println("Correct! That is your name");
+} else if (name.equals("Brian")) {
+    System.out.println("Incorrect! That is not your name");
+} else {
+    System.out.println("You clearly dont understand what a name is");
+}
+    
 
+    System.out.println("Who is the knicks best player?");
+    String player = sc.nextLine();
+if (player.equals("Jalen Brunson")){
+    System.out.println("You are correct!");
+} else if (player.equals("Julius Randle")) {
+    System.out.println("He's good, but not quite the best");
+} else {
+    System.out.println("Nope! It's Jalen Brunson, but if you said Randle than you get another guess");
+}
+
+    System.out.println("What is 5 + 5");
+    String num = sc.nextLine();
+    if (num.toLowerCase().equals("ten")) {
+        System.out.println("Correct!");
+
+     }else if (Integer.parseInt(num) == 10){
+        System.out.println("Correct");
+     }
+     else if (Integer.parseInt(num) == 25){
+        System.out.println("This is addition, not mulitplication!");
+     }
+     else {
+        System.out.println("Please re-read the question");
+     }
 
     }
-}
+
+ }
+
+
+
 
 
